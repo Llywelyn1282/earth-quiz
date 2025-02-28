2b1a6ca635f675110819aea1bda0582ebd701a8c
Llywelyn1282 <gwilymllywelyn@gmail.com>
2025-02-28 17:55:46 +0000

    add javascript functionality to generate  quiz questions

--- script.js
+++ script.js
@@ -80,3 +80,38 @@ const quizData = [
         correct: "b",
       },
     ];
+
+    const quiz = document.getElementById("quiz");
+    const answerEls = document.querySelectorAll(".answer");
+    const questionEls = document.getElementById("question");
+    const a_text = document.getElementById("a_text");
+    const b_text = document.getElementById("b_text");
+    const c_text = document.getElementById("c_text");
+    const d_text = document.getElementById("d_text");
+    const submitBtn = document.getElementById("submit");
+    const startBtn = document.getElementById("start");
+    const intro = document.getElementById("intro");
+
+    let currentQuiz = 0;
+    let score = 0;
+
+    loadQuiz();
+
+    startBtn.addEventListener("click", startGame)
+
+    function startGame() {
+        intro.style.display = "none";
+        quiz.style.display = "block";
+    }
+
+function loadQuiz() {
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+
+    questionEls.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
